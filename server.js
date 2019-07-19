bb04ef07b90e15f5cb84e2e19f8d335eaf653de5
Scott Sisil <ssisil@pivotal.io>
2019-07-19 10:59:19 -0500

    First Cloud Build Test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 3000;
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>Hello from a node app! ');
+    res.write('<h1>Hello from a node app, now with Cloud Build! ');
     res.write('via: ' + host + ':' + port);
     res.end('</h1>');
 }).listen(port, null);
